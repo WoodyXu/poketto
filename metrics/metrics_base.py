c8bc04a812631261e69c57ed887da668090f9c82
xuquan <thisiswoody.cs@gmail.com>
2018-09-03 20:49:17 +0800

    metrics base implementation

--- metrics/metrics_base.py
+++ metrics/metrics_base.py
@@ -1,6 +1,37 @@
 # -*- encoding: utf-8 -*-
 
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
 import os
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
+
+class MetricsBase(object):
+    """
+    The abstract base metrics class
+    """
+    def __init__(preds, truths, *args, **kwargs):
+        """
+        Parameters:
+            preds: a list of guess probabilities or prediction labels
+            truths: a list of ground truths
+        """
+        if len(preds) != len(truths):
+            raise ValueError("Lengths of predictions and truths must be equal, but we get {}
+                    predictions and {} ground truths!".format(len(preds), len(truths)))
+
+        # the k-v of metrics, like {'accurary': 0.8, 'recall': 0.5, ...}
+        self.metrics_d = {}
+
+    def _evaluate(self):
+        raise NotImplementedError("You must inherit MetricsBase!")
+
+    def plot(self, path_dir="/tmp/metrics"):
+        raise NotImplementedError("You must inherit MetricsBase!")
+
+    @property
+    def metrics(self):
+        return self.metrics_d
